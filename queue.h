5edf0738bc418de96e7b605bb126aa4f9168dcfb
Evin <evinodisho@gmail.com>
2025-02-15 20:16:42 -0800

    Prototypes for queue constructor and destructor

--- queue.h
+++ queue.h
@@ -4,6 +4,24 @@
 typedef struct queue queue_t;
 
 /**
+ * @brief Constructor for a new circular queue
+ * 
+ * @param size_t Maximum capacity of queue
+ * 
+ * @return queue_t* Pointer to new queue
+ */
+queue_t *NewQueue(size_t capacity);
+
+/**
+ * @brief Destructor for circular queue
+ * 
+ * @param queue_t** Address of queue_t* to delete
+ * 
+ * @return void
+ */
+void DeleteQueue(queue_t **q);
+
+/**
  * @brief Get front value from queue
  * 
  * @param queue_t* Pointer to the queue

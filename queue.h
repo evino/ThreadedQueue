82e24aab351ed45832da23998230aa29f8ff8238
Evin <evinodisho@gmail.com>
2025-02-15 20:03:36 -0800

    Header and C files

---
+++ queue.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/**
+ * @brief Get front value from queue
+ * 
+ * @param queue_t* Pointer to the queue
+ * 
+ * @return int Value at front of queue
+ */
+int GetFront();
+
+/**
+ * @brief Get back value from queue
+ * 
+ * @param queue_t* Pointer to the queue
+ * 
+ * @return int Value at back of queue
+ */
+int GetBack();
